32eb2d8bb4e055527c27994054e6dd35a80a0c40
David <dschkalee@isa-hamburg.com>
2015-06-05 14:52:43 +0200

    compile index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,12 +13,38 @@ module.exports = function(grunt) {
 					'index.html': 'index-template.html'
 				}
 			}
+		},
+
+		cssmin: {
+			target: {
+				files: {
+					'./css/style.css': ['./css/style.css']
+				}
+			}
+		},
+
+		'string-replace': {
+			dist: {
+				files: {
+					'index.html': 'index.html'
+				},
+				options: {
+					replacements: [{
+						pattern: /<!-- @import (.*?) -->/ig,
+						replacement: function (match, p1) {
+							return grunt.file.read(p1);
+						}
+					}]
+				}
+			}
 		}
 	});
 
 	// load tasks
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-string-replace');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 	// tasks
-	grunt.registerTask('default', ['htmlmin']);
+	grunt.registerTask('default', ['htmlmin', 'cssmin', 'string-replace']);
 };
